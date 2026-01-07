9acf294628a91800dbf1bc8919d8a13efb90d081
ZaraKinYu <zaraknyu@gmail.com>
2026-01-07 13:27:50 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ rm -rf vendor/xiaomi/warm
 rm -rf hardware/qcom-caf/common
 rm -rf hardware/xiaomi
 
-git clone https://github.com/zaraknyu/android_device_xiaomi_warm -b test device/xiaomi/warm
+git clone https://github.com/zaraknyu/android_device_xiaomi_warm -b lineage-23.0 device/xiaomi/warm
 git clone https://github.com/zaraknyu/android_vendor_xiaomi_warm -b lineage-23.0 vendor/xiaomi/warm
 git clone https://github.com/zaraknyu/android_device_xiaomi_warm-kernel -b lineage-23.0 device/xiaomi/warm-kernel
 git clone https://github.com/zaraknyu/android_hardware_qcom-caf_common -b lineage-23.0 hardware/qcom-caf/common
