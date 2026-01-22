6bd407a4a64405caeefd7f1fe4ac1846ac7a420b
RazenXAX <razenxizu@gmail.com>
2026-01-22 16:02:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
 /opt/crave/resync.sh
 
+rm -rf device/xiaomi/warm
 git clone https://github.com/RazenXAX/android_device_xiaomi_warm device/xiaomi/warm
 
-# Building
 source build/envsetup.sh
 lunch lineage_warm-bp2a-userdebug
 mka bacon
