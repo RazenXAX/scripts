a041ca22c51f8b1e86ad8dba38df400d99d7dcb3
ZaraKnYu <razenxizu@gmail.com>
2026-02-08 12:28:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Repo Init
-repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
 /opt/crave/resync.sh
 
 rm -rf device/xiaomi/warm
@@ -10,14 +10,14 @@ rm -rf hardware/qcom-caf/common
 rm -rf hardware/xiaomi
 rm -rf device/xiaomi/warm-kernel
 
-git clone https://github.com/zaraknyu/android_device_xiaomi_warm -b lineage-23.0 device/xiaomi/warm
-git clone https://github.com/zaraknyu/android_vendor_xiaomi_warm -b lineage-23.0 vendor/xiaomi/warm
-git clone https://github.com/zaraknyu/android_device_xiaomi_warm-kernel -b lineage-23.0 device/xiaomi/warm-kernel
-git clone https://github.com/zaraknyu/android_hardware_qcom-caf_common -b lineage-23.0 hardware/qcom-caf/common
-git clone https://github.com/lineageos/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi
+git clone https://github.com/zaraknyu/android_device_xiaomi_warm -b lineage-23.2 device/xiaomi/warm
+git clone https://github.com/zaraknyu/android_vendor_xiaomi_warm -b lineage-23.2 vendor/xiaomi/warm
+git clone https://github.com/zaraknyu/android_device_xiaomi_warm-kernel -b lineage-23.2 device/xiaomi/warm-kernel
+git clone https://github.com/zaraknyu/android_hardware_qcom-caf_common -b lineage-23.2 hardware/qcom-caf/common
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.2 hardware/xiaomi
 
 source build/envsetup.sh
-lunch lineage_warm-bp2a-userdebug
+lunch lineage_warm-bp4a-eng
 mka bacon
 
 # IF FAILED THEN THIS IS THE FIX
