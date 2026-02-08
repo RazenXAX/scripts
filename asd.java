d105636b7bb762fb453c148a522c8b6079cfc0ee
ZaraKnYu <razenxizu@gmail.com>
2026-02-08 14:36:14 +0530

    Update asd.java

--- asd.java
+++ asd.java
@@ -1,23 +1,23 @@
 "�
 ota-property-files�payload_metadata.bin:4734:258613,payload.bin:4734:7003791779,payload_properties.txt:7003796571:156,apex_info.pb:2366:1270,care_map.pb:3683:984,metadata:69:690,metadata.pb:827:1491                        "�
 ota-streaming-property-files�payload.bin:4734:7003791779,payload_properties.txt:7003796571:156,apex_info.pb:2366:1270,care_map.pb:3683:984,metadata:69:690,metadata.pb:827:1491                          *	
-peridot2�
-peridotORedmi/peridot/peridot:16/BP2A.250605.031.A3/OS3.0.5.0.WNPCNXM:user/release-keysOS3.0.5.0.WNPCNXM ����*362
+warm2�
+warmORedmi/warm/warm:15/AQ3A.240912.001/OS2.0.208.0.VNPMIXM:user/release-keysOS2.0.208.0.VNPMIXM ����*362
 2025-11-01:n
-	init_bootperidotLRedmi/peridot/peridot:14/UKQ1.240624.001/OS3.0.5.0.WNPCNXM:user/release-keys"
+	init_bootwarmLRedmi/warm/warm:14/UKQ1.240624.001/OS2.0.208.0.VNPMIXM:user/release-keys"
 1766931646:h
-odmperidotLRedmi/peridot/peridot:14/UKQ1.240624.001/OS3.0.5.0.WNPCNXM:user/release-keys"
+odmwarmLRedmi/warm/warm:14/UKQ1.240624.001/OS2.0.208.0.VNPMIXM:user/release-keys"
 1766931646:t
-product	miproductRXiaomi/peridot/miproduct:16/BP2A.250605.031.A3/OS3.0.5.0.WNPCNXM:user/release-keys"
+product	miproductRXiaomi/warm/miproduct:15/AQ3A.240912.001/OS2.0.208.0.VNPMIXM:user/release-keys"
 1766930563:k
-systemgenericLXiaomi/missi/missi:16/BP2A.250605.031.A3/OS3.0.5.0.WNPCNXM:user/release-keys"
+systemgenericLXiaomi/missi/missi:15/AQ3A.240912.001/OS2.0.208.0.VNPMIXM:user/release-keys"
 1766930542:p
-system_dlkmperidotLRedmi/peridot/peridot:14/UKQ1.240624.001/OS3.0.5.0.WNPCNXM:user/release-keys"
+system_dlkmwarmLRedmi/warm/warm:14/UKQ1.240624.001/OS2.0.208.0.VNPMIXM:user/release-keys"
 1766931646:m
 
-system_extmissiLXiaomi/missi/missi:16/BP2A.250605.031.A3/OS3.0.5.0.WNPCNXM:user/release-keys"
+system_extmissiLXiaomi/missi/missi:15/AQ3A.240912.001/OS2.0.208.0.VNPMIXM:user/release-keys"
 1766930542:y
-vendormivendorYXiaomi/mivendor_sm8650_cn/mivendor:14/UKQ1.240624.001/OS3.0.5.0.WNPCNXM:user/release-keys"
+vendormivendorYXiaomi/mivendor_sm8650_cn/mivendor:14/UKQ1.240624.001/OS2.0.208.0.VNPMIXM:user/release-keys"
 1766930568:p
-vendor_dlkmperidotLRedmi/peridot/peridot:14/UKQ1.240624.001/OS3.0.5.0.WNPCNXM:user/release-keys"
+vendor_dlkmwarmLRedmi/warm/warm:14/UKQ1.240624.001/OS2.0.208.0.VNPMIXM:user/release-keys"
 1766931646
