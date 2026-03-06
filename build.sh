1b1d28f819ae9134a9e4478a109658ef11371890
ZaraKnYu <razenxizu@gmail.com>
2026-03-06 11:44:07 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Repo Init
-repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
 /opt/crave/resync.sh
 
 rm -rf hardware/xiaomi
@@ -10,12 +10,12 @@ rm -rf hardware/qcom-caf/common
 rm -rf vendor/xiaomi/warm
 rm -rf device/xiaomi/warm
 
-git clone https://github.com/xiaomi-sm4635-resources/android_device_xiaomi_warm -b lineage-23.2 device/xiaomi/warm
+git clone https://github.com/xiaomi-sm4635-resources/android_device_xiaomi_warm -b luna device/xiaomi/warm
 git clone https://github.com/xiaomi-sm4635-resources/android_vendor_xiaomi_warm -b lineage-23.2 vendor/xiaomi/warm
 git clone https://github.com/xiaomi-sm4635-resources/android_device_xiaomi_warm-kernel -b main device/xiaomi/warm-kernel
 git clone https://github.com/xiaomi-sm4635-resources/android_hardware_qcom-caf_common -b lineage-23.2 hardware/qcom-caf/common
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.2 hardware/xiaomi
 
 source build/envsetup.sh
-lunch lineage_warm-bp4a-userdebug
-mka bacon
+lunch lineage_warm-bp4a-user
+m bacon
